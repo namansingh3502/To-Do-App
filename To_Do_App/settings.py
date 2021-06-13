4200d1099e4f827d737d96e6bdc7706c51c653de
namansingh3502 <namansingh0305@gmail.com>
2021-06-13 16:47:21 +0530

    another fix

--- To_Do_App/settings.py
+++ To_Do_App/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,5 +142,7 @@ CORS_ORIGIN_WHITELIST = [
 
     "http://localhost:3000",
     "https://dj-react-to-do-app.herokuapp.com"
-
 ]
+
+django_heroku.settings(locals())
+
