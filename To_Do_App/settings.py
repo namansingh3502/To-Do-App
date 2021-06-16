bfce91d80920598a027e7d83634be2ed3ecc792f
namansingh3502 <namansingh0305@gmail.com>
2021-06-16 23:47:23 +0530

    adding oauth

--- To_Do_App/settings.py
+++ To_Do_App/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+
+#not working on local machine due to some package issue
+#import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -41,11 +43,21 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 
     'api',
     'rest_framework',
     'corsheaders',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
+
 ]
+
+SITE_ID = 1
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -75,6 +87,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request',
             ],
         },
     },
@@ -92,6 +105,16 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+AUTHENTICATION_BACKENDS = [
+
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+
+]
 
 
 # Password validation
@@ -144,5 +167,6 @@ CORS_ORIGIN_WHITELIST = [
     "https://dj-react-to-do-app.herokuapp.com"
 ]
 
-django_heroku.settings(locals())
+#not working on local machine due to some package issue
+#django_heroku.settings(locals())
 
--- To_Do_App/urls.py
+++ To_Do_App/urls.py
@@ -18,7 +18,10 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
+
     path('', TemplateView.as_view(template_name='index.html')),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+
 ]
